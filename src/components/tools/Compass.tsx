eafb86b00e3f4077cc98756676270ac08331b532
Magcrider <harvey@magicbit.co>
2023-01-02 19:20:28 -0500

    adding the cap heading

--- src/components/tools/Compass.tsx
+++ src/components/tools/Compass.tsx
@@ -1,13 +1,28 @@
+import {useState, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-paper';
+import CompassHeading from 'react-native-compass-heading';
 const Compass = () => {
+  const [cap, setCap] = useState(0);
+  useEffect(() => {
+    const degree_update_rate = 3;
+
+    CompassHeading.start(degree_update_rate, ({heading, accuracy}) => {
+      setCap(heading);
+    });
+
+    return () => {
+      CompassHeading.stop();
+    };
+  }, []);
+
   return (
     <View style={styles.compassWrapper}>
       <View style={styles.label}>
         <Text variant="labelSmall">{`Cap Heading (°)`}</Text>
       </View>
       <View style={styles.data}>
-        <Text variant="displayLarge">{`${335}`}</Text>
+        <Text variant="displayLarge">{cap}</Text>
       </View>
     </View>
   );
